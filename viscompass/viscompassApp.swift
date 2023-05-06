ae7a75697ee6dca99111ced871cb4e81de8ea092
matt <matt@mattclark.net>
2023-05-06 13:55:54 +0800

    Initial Commit

---
+++ viscompass/viscompassApp.swift
@@ -0,0 +1,17 @@
+//
+//  viscompassApp.swift
+//  viscompass
+//
+//  Created by Matt Clark on 6/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct viscompassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
